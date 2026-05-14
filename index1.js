30532bdf2e04d66c6fab2c56ad8815e0730d017e
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-05-13 22:00:09 -0300

    Update index1.js

--- index1.js
+++ index1.js
@@ -1,7 +1,14 @@
 const input = require('readline-sync');
 
-const nome = input.question('Qual o seu nome? ');
-const idade = input.question('Qual a sua idade? ');
-const cidade = input.question('Qual a sua cidade? ');
+const num1 = Number(input.question('Digite o primeiro numero: '));
+const num2 = Number(input.question('Digite o segundo numero: '));
 
-console.log(`Olá! Meu nome é ${nome}, tenho ${idade} anos e sou de ${cidade}.`);
+console.log(`Soma: ${num1 + num2}`);
+console.log(`Subtracao: ${num1 - num2}`);
+console.log(`Multiplicacao: ${num1 * num2}`);
+
+if (num2 === 0) {
+    console.log("Erro: Não é possível dividir por zero.");
+} else {
+    console.log(`Divisao: ${num1 / num2}`);
+}

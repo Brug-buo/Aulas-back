14d09fc16780f7e4ec9d5c5a63d3b0c966462726
Bruna <bruna.valera.marques@escola.pr.gov.br>
2026-05-14 11:34:09 -0300

    atividade

--- ex3.js
+++ ex3.js
@@ -4,15 +4,13 @@ for (let i = 1; i <= 5; i++) {
     let nota = Number(input.question(`Digite a nota ${i}: `))
     soma = soma + nota
 }
-
 let media = soma / 5
-console.log(`Media final: ${media(1)}`)
-
+console.log(`Media final: ${media}`)
 
 if (media >= 7) {
     console.log("Status: Aprovado")
 } else if (media >= 5) {
-    console.log("Status: Recuperacao")
+    console.log("Recuperacao")
 } else {
-    console.log("Status: Reprovado")
+    console.log("Reprovado")
 }

4904d9722f4c0d25aa700125aa17b80ab4cf9b00
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-05-13 22:03:05 -0300

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,22 +1,17 @@
-const input = require('readline-sync');
-
-let soma = 0;
-
-// O laço 'for' vai rodar 5 vezes (de 1 ate 5)
+const input = require('readline-sync')
+let soma = 0
 for (let i = 1; i <= 5; i++) {
-    let nota = Number(input.question(`Digite a nota ${i}: `));
+    let nota = Number(input.question(`Digite a nota ${i}: `))
     soma = soma + nota; // Acumula a nota na variável soma
 }
 
 let media = soma / 5;
+console.log(`Media final: ${media.toFixed(1)}`)
 
-console.log(`Media final: ${media.toFixed(1)}`);
-
-// Lógica de aprovação
 if (media >= 7) {
-    console.log("Status: Aprovado");
+    console.log("Status: Aprovado")
 } else if (media >= 5) {
-    console.log("Status: Recuperacao");
+    console.log("Status: Recuperacao")
 } else {
-    console.log("Status: Reprovado");
+    console.log("Status: Reprovado")
 }

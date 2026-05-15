0e218ba2bce1d12cfbe2d317a418cefd1906f701
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-05-14 21:09:56 -0300

    ex2.js
    Updated subtraction and multiplication console logs to use correct accentuation.

--- ex2.js
+++ ex2.js
@@ -4,8 +4,8 @@ const num1 = Number(input.question('Digite o 1° número: '))
 const num2 = Number(input.question('Digite o 2° número: '))
 
 console.log(`Soma: ${num1 + num2}`)
-console.log(`Subtracao: ${num1 - num2}`)
-console.log(`Multiplicacao: ${num1 * num2}`)
+console.log(`Subtração: ${num1 - num2}`)
+console.log(`Multiplição: ${num1 * num2}`)
 
 if (num2 === 0) {
     console.log("Erro")

df4bd5cea531c1037111a573f4135bc7bec8ee28
Brug-buo <bruna.valera.marques@escola.pr.gov.br>
2026-05-14 21:12:12 -0300

    ex4.js

--- ex4.js
+++ ex4.js
@@ -1,7 +1,6 @@
 const readline = require('readline-sync');
 
-let numero = parseInt(readline.question('Digite um numero para ver a tabuada: '));
-
+let numero = parseInt(readline.question('Qual tabuada que deseja? '));
 console.log(`\nTabuada do ${numero}:`);
 
 for (let i = 1; i <= 10; i++) {
